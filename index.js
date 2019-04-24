c6c3e867426e31cb67d85d53fcc78a40b547ba06
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-04-24 12:14:38 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let index = 0; index < names.length; index++) {
+    console.log(`Welcome ${names[index]}! You are employee #${index + 1}.`);
+  };
+  return names;
+};
+
+
+ function tailsNeverFails() {
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  let count = 0;
+  while (maybeTrue()) {
+    count++;
+  }
+  return `You got ${count} tails in a row!`
+ }
