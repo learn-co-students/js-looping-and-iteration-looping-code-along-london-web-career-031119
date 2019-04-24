e80e30e3c0d69eefa6151a9bf2234572fda09622
peter-keller <keller.f.peter@gmail.com>
2019-04-24 08:55:38 +0100

    eh

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function printBadges(arg) {
+  for (let i = 0; i < arg.length; i++) {
+    console.log(`Welcome ${arg[i]}! You are employee #${i + 1}.`);
+  }
+  return arg;
+}
+
+function tailsNeverFails() {
+  let res = 0;
+  while (Math.random() >= 0.5) {
+    console.log(Math.random());
+    res++;
+  }
+  return `You got ${res} tails in a row!`;
+}
